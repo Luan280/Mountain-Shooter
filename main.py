216692cf53f83f51b67ed9bc6cf75f9cce954c6b
Luan Ferreira Lima <139931966+Luan280@users.noreply.github.com>
2025-06-03 18:31:48 -0300

    Update main.py
    teste git pull

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ pygame.init()
 window = pygame.display.set_mode(size=(600, 480))
 
 while True:
+    print()
     # Check for all events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
