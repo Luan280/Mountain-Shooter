6f9681a37226b66ad6a79435776a64c545c336dd
LuanFerreiraLima <luanferreiralima280@gmail.com>
2025-06-06 21:06:22 -0300

    Adding optimization from images, Adding sound in level and Creating text for 'Timeout, fps, entity'

--- code/level.py
+++ code/level.py
@@ -1,7 +1,10 @@
-from pygame import Surface
+import sys
+from pygame import Surface, Rect
+from pygame.font import Font
 import pygame
 from code.entity import Entity
 from code.entityFactory import EntityFactory
+from code.const import WIN_HEIGHT, COLOR_WHITE
 
 
 class Level:
@@ -10,13 +13,40 @@ class Level:
         self.name = name
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
-        self.entity_list.extend(EntityFactory.get_entity('Level1Bg')) # type: ignore
+        self.entity_list.extend(
+            EntityFactory.get_entity('Level1Bg'))  # type: ignore
+        self.timeout = 20000
 
-    def run(self, ):
+    def run(self):
+        pygame.mixer_music.load(f'./asset/{self.name}.mp3')
+        pygame.mixer_music.play(-1)
+        clock = pygame.time.Clock()
         while True:
+            clock.tick(60)
             for ent in self.entity_list:
                 self.window.blit(source=ent.surf, dest=ent.rect)
                 ent.move()
             pygame.display.flip()
-        pass
-        
+
+            # Check for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # Close Windown
+                    sys.exit()  # End pygame
+
+            # printed text
+            self.level_text(
+                14, f'{self.name} - Timeout: {self.timeout / 1000:.1f}s', COLOR_WHITE, (10, 5))
+            self.level_text(
+                14, f'fps: {clock.get_fps():.0f}', COLOR_WHITE, (10, WIN_HEIGHT - 35))
+            self.level_text(
+                14, f'entidades: {len(self.entity_list)}', COLOR_WHITE, (10, WIN_HEIGHT - 20))
+            pygame.display.flip()
+
+    def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
+        text_font: Font = pygame.font.SysFont(
+            name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(
+            text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
+        self.window.blit(source=text_surf, dest=text_rect)
